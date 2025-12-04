a0b1cc74a02387b2176e5639aff7038bf80c9e1e
Carlos <carloscarlesproyectodecarlos@gmail.com>
2025-12-04 23:04:21 +0100

    Nothing

---
+++ Projects/Djangobnb/bnbclone/app/layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Navbar from "./components/navbar/Navbar";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "bnb clonee",
+  description: "This is a demo app, clone of bnb",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Navbar />
+
+
+        <div className="pt-24">
+        {children}
+        </div>
+      </body>
+    </html>
+  );
+}
---
+++ Projects/Djangobnb/bnbclone/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+      <main className="">
+        <h2 className="text-airbnb">Hola con color bnb</h2>
+      </main>
+
+  );
+}
